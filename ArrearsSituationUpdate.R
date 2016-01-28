f006a77b3d740881959ee04b5e47074e552289dc
Liyang TUTU <867243454@qq.com>
2016-01-28 10:21:27 +0800

    Change file directory

--- ArrearsSituationUpdate.R
+++ ArrearsSituationUpdate.R
@@ -107,12 +107,12 @@ arrears <- function(current_date, base_date){
     
     base_pattern <- paste0("Loan_Accounts-wanzhou-liyang.qin-",base_date)
     latest_pattern <- paste0("Loan_Accounts-wanzhou-liyang.qin-",current_date)
-    base_file <- list.files("./Arrears", recursive =TRUE, all.files = TRUE, pattern = base_pattern)
-    base_file <- paste0("./Arrears/", base_file)
-    latest_file <- list.files("./Arrears", recursive = TRUE, all.files =TRUE, pattern = latest_pattern)
-    latest_file <- paste0("./Arrears/", latest_file)
-    #  classes <- c(rep("character",11), rep("numeric",13), rep("Date", 2))
-    base_data <- read.xlsx2(base_file, sheetIndex =1, header = TRUE, colClasses = classes, encoding = "urf-8")
+    base_file <- list.files("D:/360MoveData/Documents/Arrears/Mambu/", recursive =TRUE, all.files = TRUE, pattern = base_pattern)
+    base_file <- paste0("D:/360MoveData/Documents/Arrears/Mambu/", base_file)
+    latest_file <- list.files("D:/360MoveData/Documents/Arrears/Mambu/", recursive = TRUE, all.files =TRUE, pattern = latest_pattern)
+    latest_file <- paste0("D:/360MoveData/Documents/Arrears/Mambu/", latest_file)
+    # classes <- c(rep("character",11), rep("numeric",13), rep("Date", 2))
+    base_data <- read.xlsx2(base_file,sheetIndex =1, header = TRUE, colClasses = classes, encoding = "urf-8")
     latest_data <- read.xlsx2(latest_file, sheetIndex =1, header = TRUE, colClasses = classes, encoding = "urf-8")
     
     # Portfolio Observation Section#
@@ -203,14 +203,14 @@ arrears <- function(current_date, base_date){
     
     base_pattern <- paste0("Loan_Accounts-jishou-liyang.qin-",base_date)
     latest_pattern <- paste0("Loan_Accounts-jishou-liyang.qin-",current_date)
-    base_file <- list.files("./Arrears", recursive =TRUE, all.files = TRUE, pattern = base_pattern)
-    base_file <- paste0("./Arrears/", base_file)
-    latest_file <- list.files("./Arrears", recursive = TRUE, all.files =TRUE, pattern = latest_pattern)
-    latest_file <- paste0("./Arrears/", latest_file)
-    ## classes <- c(rep("character",11), rep("numeric",13), rep("Date", 2))
-    base_data <- read.xlsx2(base_file, sheetIndex =1, header = TRUE, colClasses = classes, encoding = "urf-8")
+    base_file <- list.files("D:/360MoveData/Documents/Arrears/Mambu/", recursive =TRUE, all.files = TRUE, pattern = base_pattern)
+    base_file <- paste0("D:/360MoveData/Documents/Arrears/Mambu/", base_file)
+    latest_file <- list.files("D:/360MoveData/Documents/Arrears/Mambu/", recursive = TRUE, all.files =TRUE, pattern = latest_pattern)
+    latest_file <- paste0("D:/360MoveData/Documents/Arrears/Mambu/", latest_file)
+    # classes <- c(rep("character",11), rep("numeric",13), rep("Date", 2))
+    base_data <- read.xlsx2(base_file,sheetIndex =1, header = TRUE, colClasses = classes, encoding = "urf-8")
     latest_data <- read.xlsx2(latest_file, sheetIndex =1, header = TRUE, colClasses = classes, encoding = "urf-8")
-
+    
     
     # Subset the accounts in arrears
     arrears_base <- base_data[base_data$Account.State == "In Arrears", ]
